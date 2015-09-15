10ef7ea71b29ae48ee2e40a33efb32c34da3dddd
Koncar93 <stefan.koncar@gmail.com>
2015-09-15 12:31:11 +0200

    DOdani testovi - build prolazi

---
+++ gui/tests/gui_test.c
@@ -0,0 +1,20 @@
+#include "../../unity/unity_fixture.h"
+#include "../src/gui.h"
+
+TEST_GROUP(GuiSDLTest);
+
+TEST_GROUP_RUNNER(GuiSDLTest)
+{
+//	RUN_TEST_CASE(GuiSDLTest, ____);
+
+}
+
+TEST_SETUP(BowlingTest)
+{
+	 
+}
+
+TEST_TEAR_DOWN(BowlingTest)
+{
+}
+
