2bef7d576f9246ea2e021f68abc30f1bb7f4ea52
cupostv <cupostv@gmail.com>
2015-09-15 10:56:28 +0200

    Nova polja strukture Player na zahtjev Ball_logic

--- player/src/player.h
+++ player/src/player.h
@@ -1,15 +1,24 @@
 #ifndef _PLAYER_H
 #define _PLAYER_H
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <inttypes.h>
 
-typedef struct player 
-{
-	char name[20];
-} PLAYER;
+#define LEFT_HAND 0
+#define RIGHT_HAND 1
+#define QUALITY_MIN 1
+#define QUALITY_MAX 10
 
-PLAYER* playerCreate();
+	typedef struct player 
+	{
+		char name[20];
+		uint8_t quality;
+		uint8_t main_hand;
+	} PLAYER;
+
+	PLAYER* playerCreate();
+
 #endif
 
