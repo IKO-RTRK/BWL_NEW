b905ff071788456362d947116354bdc7c170ba13
cupostv <cupostv@gmail.com>
2015-09-16 15:45:34 +0200

    Dodani komentari u Doxygen stilu.

--- player/src/player.h
+++ player/src/player.h
@@ -1,3 +1,10 @@
+/**
+@file player.h
+@brief This document contains detail description of structure PLAYER
+@author RT-RK team
+@date Septembar, 2015
+*/
+
 #ifndef _PLAYER_H
 #define _PLAYER_H
 
@@ -6,25 +13,59 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
-#define LEFT_HAND 0
+/*!
+  Represents left hand as main hand of player. When assigning main hand to player, other modules should use LEFT_HAND instead of -1.
+*/
+#define LEFT_HAND -1
+/*!
+  Represents right hand as main hand of player. When assigning main hand to player, other modules should use RIGHT_HAND instead of 1.
+*/
 #define RIGHT_HAND 1
+/*!
+  Minimum value for player quality.
+*/
 #define QUALITY_MIN 1
+/*!
+  Maximum value for player quality. 
+*/
 #define QUALITY_MAX 10
+/*!
+  Maximum length of player name. 
+*/
 #define NAME_LENGTH_MAX 20
 
-	typedef struct player 
-	{
-		char name[NAME_LENGTH_MAX];
-		uint8_t quality;
-		uint8_t main_hand;
-	} PLAYER;
+  /*! \struct player
+    Structure player is apstraction of player in game.
+  */
+  typedef struct player 
+  {
+    char name[NAME_LENGTH_MAX];	///<	Name of player in game. Used as player ID in game. See #NAME_LENGTH_MAX
+    uint8_t quality;		///<	Quality of player. Higher number means better player. See #QUALITY_MIN and #QUALITY_MAX
+    uint8_t main_hand;		///<	Main hand of player. Important for game logic. See #LEFT_HAND and #RIGHT_HAND
+  } PLAYER;
 
-	PLAYER* playerCreate();
-	void assignPlayerName(PLAYER* p, char* name);
-	void assignPlayerQuality(PLAYER* p, uint8_t quality);
-	void assignPlayerMainHand(PLAYER* p, uint8_t main_hand);
-	void increaseQuality (PLAYER* p);
-	void decreaseQuality (PLAYER* p);
+  /**
+  * @brief	This method allocates memory for structure \ref player.
+		Must be called before any other method in this file.
+  * @param	void
+  * @retval	PLAYER* Returns pointer to the allocated memory or NULL pointer if error is occured while allocating memory.
+  */
+  PLAYER* playerCreate();
+  
+  /**
+  * @brief	This method assignes \a name to the \a player.
+		If name of the player is higher than #NAME_LENGTH_MAX, the name will be cut to be exactly #NAME_LENGTH_MAX long.
+		Must be called after playerCreate(), not before.
+  * @param	PLAYER*	Pointer to structure \ref player whom we want to assign \a name.
+  * @param	char*	The name to assign to the \a player.
+  * @retval	void	
+  */
+  void assignPlayerName(PLAYER* player, char* name);
+  void assignPlayerQuality(PLAYER* player, uint8_t quality);
+  void assignPlayerMainHand(PLAYER* player, uint8_t main_hand);
+  void increaseQuality (PLAYER* player);
+  void decreaseQuality (PLAYER* player);
+  void playerDelete (PLAYER* player);
 
 #endif
 
