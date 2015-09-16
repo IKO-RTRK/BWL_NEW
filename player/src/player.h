bd6aef7457009552dc020a7b8326d79b7add7c24
cupostv <cupostv@gmail.com>
2015-09-16 11:22:38 +0200

    Napravljeni makefile-ovi. Izmjena header file-a. Zavrseno nekoliko testova i urađen refaktoring.

--- player/src/player.h
+++ player/src/player.h
@@ -10,15 +10,19 @@
 #define RIGHT_HAND 1
 #define QUALITY_MIN 1
 #define QUALITY_MAX 10
+#define NAME_LENGTH_MAX 20
 
 	typedef struct player 
 	{
-		char name[20];
+		char name[NAME_LENGTH_MAX];
 		uint8_t quality;
 		uint8_t main_hand;
 	} PLAYER;
 
 	PLAYER* playerCreate();
+	void assignPlayerName(PLAYER* p, char* name);
+	void assignPlayerQuality(PLAYER* p, uint8_t quality);
+	void assignPlayerMainHand(PLAYER* p, uint8_t main_hand);
 	void increaseQuality (PLAYER* p);
 	void decreaseQuality (PLAYER* p);
 
--- player/tests/runall.c
+++ player/tests/runall.c
@@ -1,9 +1,11 @@
 #include "../../unity/unity_fixture.h"
+
 static void RunAllTests(void)
 {
-//RUN_TEST_GROUP(Playertest);
+	RUN_TEST_GROUP(PlayerTest);
 }
+
 int main(int argc,char* argv[])
 {
-return UnityMain(argc, (const char**) argv, RunAllTests);
+	return UnityMain(argc, (const char**) argv, RunAllTests);
 }
