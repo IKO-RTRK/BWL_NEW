dd7cb07ca5eafda28f72a8f7e4f12457592da96e
sasamih <sasamihajlica@gmail.com>
2015-09-16 09:08:02 +0200

    Zapoceti testovi

---
+++ ball_logic/tests/ball_logic_test.c
@@ -0,0 +1,18 @@
+//#include "../../unity/unity_fixture.h"
+#include "../src/ball.h"
+
+TEST_GROUP(BallLogicTest)
+{
+  
+}
+
+TEST_SETUP(BallLogicTest)
+{
+  
+}
+
+TEST_TEAR_DOWN(BallLogicTest)
+{
+  
+}
+
---
+++ ball_logic/tests/ball_logic_tests.c
@@ -0,0 +1,19 @@
+#include "../../unity/unity_fixture.h"
+#include "../src/ball.h"
+
+TEST_GROUP(BallLogicTests);
+
+TEST_GROUP_RUNNER(BallLogicTests)
+{
+	
+}
+
+TEST_SETUP(BallLogicTest)
+{
+	
+}
+
+TEST_TEAR_DOWN(BallLogicTest)
+{
+	
+}
