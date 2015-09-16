a759b695ed274ed5a2865ccdc862f7402a14e9e5
Bojan Gajic <bokad@live.com>
2015-09-16 11:43:58 +0200

    Dodano par testova za f-ju knockDownPins()

--- ball_logic/src/ball.c
+++ ball_logic/src/ball.c
@@ -2,7 +2,8 @@
 
 void initBallLogic(LANE_CONFIG lane_cfg)
 {
-
+	lane.width = lane_cfg.width;
+	lane.length = lane_cfg.length;	
 }
 
 BALL_POSITION rollTheBall(struct player* theplayer, BALL_POSITION current_ball_position)
@@ -14,6 +15,10 @@ BALL_POSITION rollTheBall(struct player* theplayer, BALL_POSITION current_ball_p
 KNOCKED_DOWN_PINS knockDownPins(PLAYER* the_player, BALL_POSITION ball_position)
 {
 	KNOCKED_DOWN_PINS pins;
+	uint8_t i;
+	pins.number_of_pins = 0;
+	for (i = 0 ; i <= NUMBER_OF_PINS - 1; i++)
+		pins.pins[i] = 0;
 	return pins;
 }
 
--- ball_logic/tests/ball_logic_tests.c
+++ ball_logic/tests/ball_logic_tests.c
@@ -1,24 +1,51 @@
 #include "../../unity/unity_fixture.h"
 #include "../src/ball.h"
+#include "../../player/src/player.h"
 
-TEST_GROUP(RollTheBallTests);
 
-TEST_GROUP_RUNNER(RollTheBallBallTests)
+extern LANE_CONFIG lane;
+
+PLAYER player;
+BALL_POSITION position;
+
+
+TEST_GROUP(knockDownPins);
+
+TEST_GROUP_RUNNER(knockDownPins)
+{
+	RUN_TEST_CASE(knockDownPins, BallInLeftCanal);
+	RUN_TEST_CASE(knockDownPins, BallInRightCanal);
+}
+
+TEST_SETUP(knockDownPins)
+{
+	lane.width = 7;
+	lane.length = 40;
+}
+
+TEST_TEAR_DOWN(knockDownPins)
 {
 	
 }
 
-TEST_SETUP(RollTheBallBallTest)
+TEST(knockDownPins, BallInLeftCanal)
 {
-	
+	uint8_t i;
+	position.x = - lane.width/2 - 1;
+	KNOCKED_DOWN_PINS pins = knockDownPins(&player, position);
+	for(i = 0; i <= NUMBER_OF_PINS - 1; i++)
+	{
+		TEST_ASSERT_EQUAL(0, pins.pins[i]);
+	}
 }
 
-TEST_TEAR_DOWN(RollTheBallBallTest)
+TEST(knockDownPins, BallInRightCanal)
 {
-	
-}
-
-TEST(RollTheBallBallTest, StraightLineTest)
-{
-  
+	uint8_t i;
+	position.x = lane.width/2 + 1;
+	KNOCKED_DOWN_PINS pins = knockDownPins(&player, position);
+	for(i = 0; i <= NUMBER_OF_PINS - 1; i++)
+	{
+		TEST_ASSERT_EQUAL(0, pins.pins[i]);
+	}
 }
