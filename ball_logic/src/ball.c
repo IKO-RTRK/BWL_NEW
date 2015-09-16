0c68898f5c16affc000b66a19d8f18993e3618e9
sasamih <sasamihajlica@gmail.com>
2015-09-16 12:38:57 +0200

    Inicijalni test za rollTheBall

--- ball_logic/src/ball.c
+++ ball_logic/src/ball.c
@@ -2,12 +2,14 @@
 
 void initBallLogic(LANE_CONFIG lane_cfg)
 {
-
+  lane.width = lane_cfg.width;
+  lane.length = lane_cfg.length;
 }
 
 BALL_POSITION rollTheBall(struct player* theplayer, BALL_POSITION current_ball_position)
 {
 	BALL_POSITION next_ball_position;
+	next_ball_position.x = 0;//current_ball_position.coeff * current_ball_position.y + current_ball_position.x;
 	return next_ball_position;
 }
 
--- ball_logic/tests/ball_logic_tests.c
+++ ball_logic/tests/ball_logic_tests.c
@@ -1,24 +1,53 @@
 #include "../../unity/unity_fixture.h"
 #include "../src/ball.h"
+#include <stdlib.h>
+#include <inttypes.h>
+#include <stdint.h>
 
 TEST_GROUP(RollTheBallTests);
 
-TEST_GROUP_RUNNER(RollTheBallBallTests)
+TEST_GROUP_RUNNER(RollTheBallTests)
+{
+    RUN_TEST_CASE(RollTheBallTest, StraightLineTest);
+   // RUN_TEST_CASE(RollTheBallTest, OffsetStraightLineTest);
+}
+
+TEST_SETUP(RollTheBallTest)
+{
+    LANE_CONFIG lane2;
+    lane2.width = 20;
+    lane2.length = 50;
+    initBallLogic(lane2);
+}
+
+TEST_TEAR_DOWN(RollTheBallTest)
 {
 	
 }
 
-TEST_SETUP(RollTheBallBallTest)
+TEST(RollTheBallTest, StraightLineTest)
 {
-	
-}
-
-TEST_TEAR_DOWN(RollTheBallBallTest)
-{
-	
-}
-
-TEST(RollTheBallBallTest, StraightLineTest)
-{
+  int i;
   
+  uint32_t expected[lane.length];
+  for (i = 0; i < lane.length; i++)
+  {
+    expected[i] = 0;
+  }
+  
+  uint32_t positions[lane.length];
+  BALL_POSITION ball_pos;
+  BALL_POSITION ball_pos_next;
+  positions[0] = 0;
+  
+  for(i = 0;i < lane.length;i++)
+  {
+      ball_pos.y = i;
+      ball_pos.x = 0;
+      ball_pos_next = rollTheBall(NULL,ball_pos);
+      positions[i+1] = ball_pos_next.x; 
+  }
+  
+  TEST_ASSERT_EQUAL_UINT32_ARRAY(expected, positions, lane.length);
 }
+
