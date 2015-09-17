29b6dcad09ff3f855f39d4eb5891b182d55bcf74
NikolaNikolicRTRK <nikmusic@live.com>
2015-09-17 11:53:08 +0200

    Initialisation functions added

--- gui/src/gui.c
+++ gui/src/gui.c
@@ -7,6 +7,19 @@ static SDL_Surface* bowling_lane = NULL;
 static uint16_t prevX[3];
 static uint16_t prevY[3];
 
+static void initialisation_track_console(TRACK_CONSOLE* t, BOWLING_GAME* the_game)
+static void initialisation_pins_console(TRACK_CONSOLE* track);
+static void initialisation_lane_console(TRACK_CONSOLE* track);
+static void initialisation_table_console(TRACK_CONSOLE* track, BOWLING_GAME* the_game);
+
+static void print_lane_console(TRACK_CONSOLE* track);
+static void print_table_console(TRACK_CONSOLE* track);
+
+
+
+
+
+
 static uint8_t drawKnockedPinsAndTable_console(BOWLING_GAME* the_game, uint8_t current_player, KNOCKED_DOWN_PINS knocked_down_pins);
 static uint8_t animateBallMovement_console(BOWLING_GAME* the_game, uint8_t current_player, BALL_POSITION ball_position);
 
@@ -24,8 +37,9 @@ uint8_t initGUI(uint8_t gui)
 	{
 		drawKnockedPinsAndTable = NULL;
 		animateBallMovement = NULL;
-		// configure my_lane_config based on GUI choose (width/length/...)
-		
+		my_lane_config.length=LENGTH_OF_LANE_CONSOLE - START_COLUMN_PINS;
+		my_lane_config.width=WIDTH_OF_LANE_CONSOLE-2;
+
 	} 
 	else
 	{
@@ -66,6 +80,200 @@ uint8_t initGUI(uint8_t gui)
 
 
 // CONSOLE GUI START
+
+static void initialisation_track_console(TRACK_CONSOLE* t, BOWLING_GAME* the_game)
+{
+	initialisation_pins_console(t);
+	initialisation_lane_console(t);
+	initialisation_table_console(t, the_game);
+}
+static void initialisation_lane_console(TRACK_CONSOLE* track)
+{
+	uint8_t i,j;
+	uint8_t current_row;
+	uint8_t current_column;
+	
+	for (i = 0; i < LENGTH_OF_LANE_CONSOLE; i++) 
+	{
+	  for (j = 0; j < WIDTH_OF_LANE_CONSOLE; j++)
+	  {
+	    track->table_gui[i][j] = ' ';
+	  }
+	}
+	track->ball_prevXpos=FIRST_BALL_POS_COL;
+	track->ball_prevYpos=FIRST_BALL_POS_ROW;
+
+	for (current_row = START_OF_FIRST_TRACK_ROW ; current_row < LENGTH_OF_LANE_CONSOLE; current_row++)
+	{
+		track->lane_gui[current_row][2] = '|';
+		track->lane_gui[current_row][4] = '|';
+		track->lane_gui[current_row][12] = '|';
+		track->lane_gui[current_row][14] = '|';
+	}
+    
+	track->lane_gui[FIRST_BALL_POS_ROW][FIRST_BALL_POS_COL] = bowling_ball; 
+	return;
+}
+
+static void initialisation_pins_console(TRACK_CONSOLE* track)
+{
+    uint8_t current_row;
+    uint8_t current_column;
+    uint8_t counter = NUM_OF_PINS - 1;
+    uint8_t tmp = START_OF_FIRST_TRACK_ROW, tmp1;
+    uint8_t n = START_COLUMN_PINS;
+
+    for (current_row =END_OF_PINS_ROW; current_row > START_OF_FIRST_TRACK_ROW; current_row--)
+    {
+	tmp1 = n;
+	for (current_column = current_row; current_column >START_OF_FIRST_TRACK_ROW; current_column--)
+	{
+	    track->lane_gui[tmp][tmp1] = bowling_pins[counter--];
+	    tmp1+=2;
+	}
+	tmp++;
+	n++;
+	
+    }
+    return;
+}
+
+static void initialisation_table_console(TRACK_CONSOLE* track, BOWLING_GAME* the_game)
+{   
+	uint8_t i, j;
+	uint8_t diff = 0;
+	uint8_t temp = 0;
+	char num = '1';
+
+	for (i = 0; i < LENGTH_OF_TABLE_CONSOLE; i++) 
+	{
+	  for (j = 0; j < WIDTH_OF_TABLE_CONSOLE; j++)
+	  {
+	    track->table_gui[i][j] = ' ';
+	  }
+	}
+	
+	for (i = 0; i < 10; i++)
+	{
+	  track->table_gui[0][diff] = '|';
+	  diff+=4;
+	}
+	
+	
+	track->table_gui[0][42] = '|';
+	track->table_gui[0][48] = '|';
+	
+	diff = 2;
+	
+	for (i = 0; i < 9; i++)
+	{
+	    track->table_gui[0][diff] = num;
+	    num+=1;
+	    diff += 4;
+	}
+	
+	track->table_gui[0][38] = '1';
+	track->table_gui[0][39] = '0';
+	
+	track->table_gui[0][44] = 'T';
+	track->table_gui[0][45] = 'O';
+	track->table_gui[0][46] = 'T';
+	
+	while(temp < the_game->number_of_players)
+	{		
+		uint8_t current_column;
+		uint8_t tmp1 = START_LANE_ROW;			
+		uint8_t counter;
+		for (counter = 0; counter < 9; counter++)
+		{
+			for (current_column = 1; current_column < 4; current_column++) 
+			{
+				track->table_gui[1 + DIFF_TABLE*temp][tmp1 + current_column] = '-';
+				track->table_gui[4 + DIFF_TABLE*temp][tmp1 + current_column] = '-';
+			}
+			
+			tmp1+=4;
+		 }
+		 
+		 for (counter = 0; counter < 2; counter++)
+		{
+			for (current_column = 1; current_column < 6; current_column++) 
+			{
+				track->table_gui[1 + DIFF_TABLE*temp][tmp1 + current_column] = '-';
+				track->table_gui[4 + DIFF_TABLE*temp][tmp1 + current_column] = '-';
+			}
+			
+			tmp1+=6;
+		 }
+		
+		 uint8_t tmp = START_LANE_COLUMN;		
+		 for (counter = 0; counter < 10; counter++)
+		 {
+			for (current_column = 0; current_column < 2; current_column++) 
+			{
+				track->table_gui[2 + DIFF_TABLE*temp][tmp] = '|';
+				track->table_gui[3 + DIFF_TABLE*temp][tmp] = '|';
+			}
+			tmp+=4;
+		 }
+
+ 	       track->table_gui[2 + DIFF_TABLE*temp][42] = '|';	
+	       track->table_gui[3 + DIFF_TABLE*temp][42] = '|';
+	       track->table_gui[2 + DIFF_TABLE*temp][48] = '|';
+	       track->table_gui[3 + DIFF_TABLE*temp][48] = '|';
+		
+	  for (i = 1; the_game->players[temp].name[i-1] != '\0'; i++)
+          {
+	   track->table_gui[5 + DIFF_TABLE*temp][i] =the_game->players[temp].name[i-1];
+	  }
+				    
+         	temp++;
+	}				    
+}
+
+static void print_lane_console(TRACK_CONSOLE* track)
+{
+	uint8_t current_row;
+	uint8_t current_column;
+
+
+	for (current_row = 0; current_row < LENGTH_OF_LANE_CONSOLE; current_row++)
+	{
+		for (current_column = 0; current_column < WIDTH_OF_LANE_CONSOLE; current_column++)
+		{
+
+			SetCursorPos( (track->trackID-1) * DIFF_CONSOLE + current_column, current_row + 1);
+
+			printf("%c", track->lane_gui[current_row][current_column]);
+
+		}
+	}
+
+	return;
+}
+
+
+
+static void print_table_console(TRACK_CONSOLE* track)
+{
+
+	uint8_t current_row;
+	uint8_t current_column;
+	
+	for (current_row = 0 ; current_row < LENGTH_OF_TABLE_CONSOLE ; current_row++) 
+	{
+	    for (current_column = 0 ; current_column < WIDTH_OF_TABLE_CONSOLE ; current_column++)
+	    {
+		  SetCursorPos(((track->trackID-1) * DIFF_CONSOLE) + current_column + 16, current_row);
+		  printf("%c", track->table_gui[current_row][current_column]);
+	    }
+	}
+	
+	return;
+}
+
+
+
 static uint8_t drawKnockedPinsAndTable_console(BOWLING_GAME* the_game, uint8_t current_player, KNOCKED_DOWN_PINS knocked_down_pins)
 {
 
--- gui/src/gui.h
+++ gui/src/gui.h
@@ -16,11 +16,36 @@
 #define BALL_MIDDLE 15
 #define TWO_LANES_DISTANCE 350
 
+#define LENGTH_OF_LANE_CONSOLE 35    
+#define WIDTH_OF_LANE_CONSOLE 15     
+#define LENGTH_OF_TABLE_CONSOLE 37   
+#define WIDTH_OF_TABLE_CONSOLE 49    
+#define FIRST_BALL_POS_COL 8         
+#define FIRST_BALL_POS_ROW 34        
+#define NUM_OF_PINS 10		     
+#define START_LANE_ROW 0             
+#define START_LANE_COLUMN 0         
+#define START_COLUMN_PINS 5          
+#define END_OF_PINS_ROW 4	     
+#define START_OF_FIRST_TRACK_ROW 0   
+#define DIFF_CONSOLE 66
+#define DIFF_TABLE 6
+
 typedef enum
 {
 	CONSOLE = 0,
 	SDL	= 1
 } GUI_TYPE;
+
+typedef struct track_console
+{
+	uint8_t trackID;
+	uint8_t	ball_prevXpos;
+	uint8_t ball_prevYpos;
+	char lane_gui[LENGTH_OF_LANE_CONSOLE][WIDTH_OF_LANE_CONSOLE];
+	char table_gui[LENGTH_OF_TABLE_CONSOLE][WIDTH_OF_TABLE_CONSOLE];
+	
+}TRACK_CONSOLE;
 
 extern LANE_CONFIG my_lane_config;
 
