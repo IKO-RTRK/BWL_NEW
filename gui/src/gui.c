1c899081567bc485dfc6cfc2e0dd5f580a9270f0
Koncar93 <stefan.koncar@gmail.com>
2015-09-17 02:09:07 +0200

    3. TEST (Animacija sa vise kugli)

--- gui/src/gui.c
+++ gui/src/gui.c
@@ -4,6 +4,9 @@ static SDL_Surface* screen = NULL;
 static SDL_Surface* ball[2] = {NULL, NULL};  
 static SDL_Surface* bowling_lane = NULL; 
 
+static uint16_t prevX[3];
+static uint16_t prevY[3];
+
 static uint8_t drawKnockedPinsAndTable_console(BOWLING_GAME* the_game, uint8_t current_player, KNOCKED_DOWN_PINS knocked_down_pins);
 static uint8_t animateBallMovement_console(BOWLING_GAME* the_game, uint8_t current_player, BALL_POSITION ball_position);
 
@@ -11,6 +14,7 @@ static uint8_t drawKnockedPinsAndTable_SDL(BOWLING_GAME* the_game, uint8_t curre
 static uint8_t animateBallMovement_SDL(BOWLING_GAME* the_game, uint8_t current_player, BALL_POSITION ball_position);
 
 static uint8_t printLane(uint8_t i);
+static void printBall(uint16_t x, uint16_t y, uint8_t lane);
 
 LANE_CONFIG my_lane_config;
 
@@ -49,6 +53,12 @@ uint8_t initGUI(uint8_t gui)
 			if (printLane(i))	
 			return 3;
 		}
+
+		Uint32 colorkey1 = SDL_MapRGB( ball[0] -> format, 255, 234, 157);
+		SDL_SetColorKey(ball[0], SDL_SRCCOLORKEY, colorkey1);
+
+		Uint32 colorkey2 = SDL_MapRGB( ball[1] -> format, 255, 234, 157);
+		SDL_SetColorKey(ball[1], SDL_SRCCOLORKEY, colorkey2);
 		
 		return 0;
 	}
@@ -91,8 +101,42 @@ static uint8_t drawKnockedPinsAndTable_SDL(BOWLING_GAME* the_game, uint8_t curre
 
 static uint8_t animateBallMovement_SDL(BOWLING_GAME* the_game, uint8_t current_player, BALL_POSITION ball_position)
 {
-	
-	
+	printBall(ball_position.x, ball_position.y, the_game -> lane_number);
+	return 0;
+}
+
+static void printBall(uint16_t x, uint16_t y, uint8_t lane)  // x,y - centar lopte
+{
+
+	// Pozicija na stazi na kojoj se brise lopta
+	SDL_Rect dstOffsetDel; 
+	dstOffsetDel.x = INIT_OFFSET_X + prevX[lane] + (lane) * TWO_LANES_DISTANCE; 
+	dstOffsetDel.y = INIT_OFFSET_Y + LANE_HEIGHT - BALL_DIM - prevY[lane];
+
+	SDL_Rect srcOffsetDel;
+
+	srcOffsetDel.w = BALL_DIM; 
+	srcOffsetDel.h = BALL_DIM;
+	srcOffsetDel.x = prevX[lane]; 
+	srcOffsetDel.y = LANE_HEIGHT - BALL_DIM - prevY[lane];
+	// Brisanje prethodno iscrtane lopte
+	if (y != 0)
+	{
+		SDL_BlitSurface(bowling_lane, &srcOffsetDel, screen, &dstOffsetDel);
+	}
+	// Pozicija na stazi na koju se stavlja lopta
+	SDL_Rect dstOffset; 
+	dstOffset.x = INIT_OFFSET_X + x + (lane) * TWO_LANES_DISTANCE; 
+	dstOffset.y = INIT_OFFSET_Y + LANE_HEIGHT - BALL_DIM - y;
+
+	SDL_Surface* tmp = (y % 30 == 0) ? ball[0] : ball[1];
+
+	SDL_BlitSurface(tmp, NULL, screen, &dstOffset);
+
+	SDL_Flip(screen);
+
+	prevX[lane] = x;
+	prevY[lane] = y;
 }
 
 void quit()
--- gui/tests/gui_test.c
+++ gui/tests/gui_test.c
@@ -1,8 +1,9 @@
 #include "../../unity/unity_fixture.h"
 #include "../src/gui.h"
+#include "../../include/SDL.h"
 
-BOWLING_GAME bg;
-BALL_POSITION bp;
+BOWLING_GAME bg1, bg2;
+BALL_POSITION bp1, bp2;
 
 TEST_GROUP(SDLAnimationTest);
 
@@ -11,6 +12,7 @@ TEST_GROUP_RUNNER(SDLAnimationTest)
 {
 	RUN_TEST_CASE(SDLAnimationTest, SDL_Init);
 	RUN_TEST_CASE(SDLAnimationTest, BMP_Load);
+	RUN_TEST_CASE(SDLAnimationTest, Animation);
 }
 
 TEST_SETUP(SDLAnimationTest)
@@ -33,4 +35,28 @@ TEST(SDLAnimationTest, BMP_Load)
 {
 	TEST_ASSERT_EQUAL(0, initGUI(SDL));
 }
+
+// Treci test - Animacija
+TEST(SDLAnimationTest, Animation)
+{
+	initGUI(SDL);
+	bp1.x = 0;
+	bp2.x = 50;
+	bg1.lane_number = 0;
+	bg2.lane_number = 2;
+	uint8_t i;
+	for (i = 0; i < 250; i++)
+	{	
+		SDL_Delay(10);
+		bp1.y = i;
+		bp2.y = i;
+		animateBallMovement(&bg1, 0, bp1);
+		animateBallMovement(&bg2, 0, bp2);
+	}
+	SDL_Delay(4000);
+	TEST_ASSERT_EQUAL(0, 0);
+}
+
+
+
 
