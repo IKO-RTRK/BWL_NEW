aa44f3d32ac4e37f8f820fd259a7b945bad736d0
SinisaDjuric <sinisadjuric93@yahoo.com>
2015-09-17 08:45:04 +0200

    Dodani jos neki testovi

--- player/src/player.c
+++ player/src/player.c
@@ -1,5 +1,9 @@
 #include "player.h"
 
+static int isQualityInBounds(uint8_t quality)
+{
+	return ((quality >= QUALITY_MIN) && (quality <= QUALITY_MAX));
+}
 
 PLAYER* playerCreate()
 {
@@ -12,3 +16,26 @@ void assignPlayerName(PLAYER* player, char* name)
 	player -> name[NAME_LENGTH_MAX-1] = '\0';
 }
 
+void assignPlayerQuality(PLAYER* player, uint8_t quality)
+{
+	if (isQualityInBounds (quality))
+	{
+		player -> quality = quality;
+	}
+	else
+	{
+		player -> quality = 1;
+	}
+}
+
+void assignPlayerMainHand(PLAYER* player, uint8_t main_hand)
+{
+	if ((main_hand == 0) || (main_hand == 1))
+	{
+		player -> main_hand = main_hand;
+	}
+	else
+	{
+		player -> main_hand = 1;
+	}
+}
--- player/tests/player_test.c
+++ player/tests/player_test.c
@@ -10,6 +10,10 @@ TEST_GROUP_RUNNER(PlayerTest)
 	RUN_TEST_CASE (PlayerTest, TestAllocatePlayer);
 	RUN_TEST_CASE (PlayerTest, TestCreateName);
 	RUN_TEST_CASE (PlayerTest, TestNameOutOfBounds);
+	RUN_TEST_CASE (PlayerTest, AssignedQualityOutOfBounds);
+	RUN_TEST_CASE (PlayerTest, AssignedQualityInBounds);
+	RUN_TEST_CASE (PlayerTest, AssignedMainHandOutOfBounds);
+	RUN_TEST_CASE (PlayerTest, AssignedMainHandInBounds);
 }
 
 TEST_SETUP(PlayerTest)
@@ -37,3 +41,28 @@ TEST(PlayerTest, TestNameOutOfBounds)
 	assignPlayerName (player, "name123456789123456789");
 	STRCMP_EQUAL("name123456789123456", player->name);
 }
+
+TEST(PlayerTest, AssignedQualityOutOfBounds)
+{
+	assignPlayerQuality (player, 0);
+	TEST_ASSERT_TRUE (player -> quality == 1);
+}
+
+TEST(PlayerTest, AssignedQualityInBounds)
+{
+	assignPlayerQuality (player, 7);
+	TEST_ASSERT_TRUE (player -> quality == 7);
+}
+
+TEST(PlayerTest, AssignedMainHandOutOfBounds)
+{
+	assignPlayerMainHand (player, 7);
+	TEST_ASSERT_TRUE (player -> main_hand == 1);
+}
+
+TEST(PlayerTest, AssignedMainHandInBounds)
+{
+	assignPlayerMainHand (player, 0);
+	TEST_ASSERT_TRUE (player -> main_hand == 0);
+}
+
