721f09e7e8c275480ae30398c9556790336e8c41
Asim Sabic <asim2809sabic@hotmail.com>
2015-09-17 08:58:21 +0200

    Prolaze 4 testa

--- player/src/player.c
+++ player/src/player.c
@@ -8,7 +8,14 @@ PLAYER* playerCreate()
 
 void assignPlayerName(PLAYER* player, char* name)
 {
-	strncpy (player -> name, name, NAME_LENGTH_MAX);
-	player -> name[NAME_LENGTH_MAX-1] = '\0';
+	if(strlen(name) < 1)
+	{
+		strcpy (player -> name, "Player");
+	}	
+	else
+	{
+		strncpy (player -> name, name, NAME_LENGTH_MAX);
+		player -> name[NAME_LENGTH_MAX-1] = '\0';
+	}
 }
 
--- player/tests/player_test.c
+++ player/tests/player_test.c
@@ -10,6 +10,7 @@ TEST_GROUP_RUNNER(PlayerTest)
 	RUN_TEST_CASE (PlayerTest, TestAllocatePlayer);
 	RUN_TEST_CASE (PlayerTest, TestCreateName);
 	RUN_TEST_CASE (PlayerTest, TestNameOutOfBounds);
+	RUN_TEST_CASE (PlayerTest, TestEmptyName);
 }
 
 TEST_SETUP(PlayerTest)
@@ -36,4 +37,10 @@ TEST(PlayerTest, TestNameOutOfBounds)
 {
 	assignPlayerName (player, "name123456789123456789");
 	STRCMP_EQUAL("name123456789123456", player->name);
+}
+
+TEST(PlayerTest, TestEmptyName)
+{
+	assignPlayerName (player,"");
+	STRCMP_EQUAL("Player", player->name);
 }
