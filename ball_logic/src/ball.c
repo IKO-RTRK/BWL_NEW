1d41dae2b71a7d9f016a14e9d9b074262e2c8c7d
sasamih <sasamihajlica@gmail.com>
2015-09-18 08:48:26 +0200

    Lijeva i desna ruka

--- ball_logic/src/ball.c
+++ ball_logic/src/ball.c
@@ -66,76 +66,6 @@ BALL_POSITION rollTheBall(struct player* the_player, BALL_POSITION current_ball_
 	return next_ball_position;
 }
 
-// ==========================================================================================
-
-
-typedef struct node{ // lista koja sluzi za izvlacenje rednog broja dostupnih pinova koji se mogu srusiti
-	uint8_t position;
-	struct node *next;
-} node;
-
-static node * head=NULL;
-static node * tail=NULL;
-static uint8_t listSize=0;
-
-static void listAppend(uint8_t broj)
-{
-	if ( head == NULL ) 
-	{
-		head=tail=(node*)malloc(sizeof(node));
-		head->position=tail->position=broj;
-		head->next=tail->next=NULL;
-		return;
-	}
-	node *temp=tail;
-	tail=(node*)malloc(sizeof(node));
-	tail->next=NULL;
-	temp->next=tail;
-	tail->position=broj;
-}
-
-static void listDestroy()
-{
-	node *temp;
-	while(head!=NULL)
-	{
-		temp=head->next;
-		free(head);
-		head=temp;
-	}
-	head=tail=NULL;
-	listSize=0;
-}
-
-static uint8_t listGetAvailablePin(uint8_t atPosition) // vraca pin koji se moze srusiti, a nalazi se na poziciji "atPosition" u listi
-{
-	if ( atPosition > listSize || atPosition < 1 ) atPosition=1; // u slucaju da dodje nedozvoljena vrijednost izbacuje se onaj na poziciji 1
-	uint8_t i=1;
-	node *temp=head;
-	node *prev=NULL;
-	for(; i<atPosition; i++)
-	{
-		prev=temp;
-		temp=temp->next;
-	}
-	i=temp->position;
-	if ( temp==head ) { head=head->next; free(temp); listSize--; return i; }
-	if ( temp==tail ) { free(tail); tail=prev; tail->next=NULL; listSize--; return i; }
-	prev->next=temp->next;
-	free(temp);
-	listSize--;
-	return i;
-	
-}
-static void listInitialisation()
-{
-	uint8_t i;
-	listDestroy();
-	for(i=0; i <= NUMBER_OF_PINS - 1; i++)
-		listAppend(i);
-	listSize=10;
-}
-
 static double power_f(double x,uint8_t n)
 {
 	uint8_t i;
@@ -172,59 +102,30 @@ static uint8_t howManyToKnockMax(int32_t position)
 
 static void resetKnockedDownpins(KNOCKED_DOWN_PINS *pins)
 {
-	pins->number_of_pins = 0;
 	uint8_t i;
+	
+	pins->number_of_pins = 0;
+	
 	for(i=0; i <= NUMBER_OF_PINS - 1 ; i++)
 	{
 		pins->pins[i] = 0;
 	}
 }
 
-
-static uint32_t randomNumber1()
-{
-	static uint32_t coeff = 19 ;
-	uint32_t rand = rand*coeff + 3;
-	coeff = coeff + 5;
-	return rand;
-}
-static uint32_t randomNumber2()
-{
-	return random();
-}
-
 KNOCKED_DOWN_PINS knockDownPins(PLAYER* the_player, BALL_POSITION ball_position)
 {
 	KNOCKED_DOWN_PINS pins;
 	uint8_t i, max;
-
 	resetKnockedDownpins(&pins);
 	max = howManyToKnockMax((int32_t)ball_position.x);
 
-	uint32_t (*randomNumber)();
-	static uint32_t which = 0;
-	which++;
-	
-	if ( which % 2 ) randomNumber = randomNumber1;
-		else randomNumber = randomNumber2;
-
 	if ( max == 0 ) pins.number_of_pins =  0; 
-	else pins.number_of_pins = max - ( randomNumber() % max );
-
-	listInitialisation();
+	else pins.number_of_pins = max - ( random() % max );
 	//printf("%"PRIu8" ", pins.number_of_pins);
 
 	for(i=0; i < pins.number_of_pins ; i++)
 	{
-		if ( listSize == 0 ) pins.pins[listGetAvailablePin(1)] = 1;
-			else pins.pins[listGetAvailablePin( ( randomNumber() % listSize ) + 1 )] = 1;
+		pins.pins[i] = 1;
 	}
-
-	/*for(i=0; i < NUMBER_OF_PINS ; i++)
-	{
-		printf("%"PRIu8" ", pins.pins[i]);
-	}
-	printf("\n\n\n");*/
-	listDestroy();
 	return pins;
 }
